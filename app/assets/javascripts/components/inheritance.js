799300088d4e6c42fbf6ccc824c2351f9100b40f
thuannv155632 <thuan.nv@neo-lab.vn>
2017-03-24 17:16:14 +0700

    aaa

--- app/assets/javascripts/components/inheritance.js
+++ app/assets/javascripts/components/inheritance.js
@@ -15,6 +15,7 @@ class Inheritance extends ModuleClass {
 		// arrow function
 		setTimeout(() => {
 	    console.log(this.newarg); // 3
+	    console.log('====end-asynchronously======')
 	  }, 1000)
 
 		// traditional function
@@ -23,7 +24,9 @@ class Inheritance extends ModuleClass {
 	  }, 1000)
 	}
 
-	// see [Symbol.iterator]() at http://freetuts.net/iterables-va-iterators-trong-es6-622.html
+	// see [Symbol.iterator]() and yield at http://freetuts.net/iterables-va-iterators-trong-es6-622.html
+	// http://qnimate.com/javascript-yield-keyword-and-function-syntax/
+	// https://kipalog.com/posts/function--va-yield-trong-Javascript-generator-function
 	for_of_loop() {
 		return {
 	    elements: [5, 1, 4, 6, 9, 4, 54],
@@ -53,4 +56,50 @@ class Inheritance extends ModuleClass {
 	// z.next() => Object {value: 4, done: false}
 	// z.next() => Object {value: 6, done: false}
 	// z.next() => Object {value: undefined, done: true}
+
+	// Set tutorial https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+	set_tutor() {
+		let set = new Set()
+		//add three keys to the set
+		set.add({x: 12})
+		set.add(44)
+		set.add("text")
+		//check if a provided key is present
+		console.log('set.has("text")')
+		console.log(set.has("text"))
+
+		//delete a key
+		console.log('set.delete(44)')
+		set.delete(44)
+		console.log('set.has(44)')
+		console.log(set.has(44))
+
+		for(var i of set)	{
+		    console.log(i)
+		}
+
+		//create a set from array values
+		let set_1 = new Set([1, 2, 3, 4, 5])
+		//size of set
+		console.log(set_1.size); //5
+
+		//create a clone of another set
+		var set_2 = new Set(set.values())
+		console.log(set_2)
+
+		// Iterating Sets => Object {x: 12} and text
+		for (let item of [...set]) {
+			console.log(item)
+		}
+
+		// Same above
+		for (let item of set) {
+			console.log(item)
+		}
+
+		// [...set] => [Object {x: 12}, "text"]
+		console.log([...set])
+
+		// Set vs Weekset http://qnimate.com/difference-between-set-and-weakset-in-javascript/
+	}
 }
--- app/assets/javascripts/components/main.js
+++ app/assets/javascripts/components/main.js
@@ -1,54 +1,57 @@
 'use strict'
 
 $(document).on('turbolinks:load', () => {
-	console.log('==============================')
+	console.log('============start=============')
 	let moduleClass = new ModuleClass(5, 100)
 
 	let valMClass = moduleClass.returnArg
 	console.log(valMClass)
-	console.log('==============================')
+	console.log('===========getter=============')
 
 	moduleClass._arg1 = 8
 	console.log(moduleClass._arg1)
-	console.log('==============================')
+	console.log('==========setter==============')
 
 	let absArg = moduleClass.absArg(111)
 	console.log(absArg)
-	console.log('==============================')
+	console.log('===========absArg=============')
 
 	let rdArr = moduleClass.randomArray
 	console.log(rdArr)
-	console.log('==============================')
+	console.log('==========randomArray=========')
 
 	console.log(moduleClass.test())
-	console.log('==============================')
+	console.log('=========normal-func==========')
 
 	let arrowFunc = () => { console.log(1000) }
 	arrowFunc()
 	console.log('==============================')
 
 	moduleClass.missArgument(undefined, 7, 9)
-	console.log('==============================')
+	console.log('========missArgument==========')
 
 	let array = [2, 4, 7, 0]
 	moduleClass.multiParams(undefined, 7, array)
-	console.log('==============================')
+	console.log('=========multiParams==========')
 
 	let valInheritance = new Inheritance(1, 2, 3)
 	console.log(valInheritance._arg1)
-	console.log('==============================')
+	console.log('==========Inheritance=========')
 
 	let a, b
 	valInheritance.sum(a = () => 20 + 10, b = () => 1 + 56)
 	valInheritance.sum(() => 20 + 10, () => 1 + 56)
 	valInheritance.sum(a => 20 + 10, b => 1 + 56)
-	console.log('==============================')
+	console.log('=========sum-arrow_func=======')
 
 	valInheritance.asynchronously()
-	console.log('==============================')
+	console.log('====start-asynchronously======')
 
 	for(var i of valInheritance.for_of_loop())	{
     console.log(i); // 5, 1, 4, 6
 	}
-	console.log('==============================')
+	console.log('=========for_of_loop==========')
+
+	valInheritance.set_tutor()
+	console.log('==========set_tutor===========')
 })
